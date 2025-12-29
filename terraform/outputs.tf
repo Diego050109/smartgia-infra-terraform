796f60b82a9362c5ea31f6f665bb85dbe20673f2
Diego050109 <lemadiego597@gmail.com>
2025-12-28 21:31:02 -0500

    feat: create vpc subnets and igw

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,21 @@
 output "region" {
   value = var.aws_region
 }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+}
+
+output "private_subnets" {
+  value = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id
+  ]
+}
